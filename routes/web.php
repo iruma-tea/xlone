51e4b520fb0086825cc1cf20f03aea68eec46757
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-06-02 18:48:46 +0900

    PostControllerの追加

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('posts', [PostController::class, 'index'])->middleware('auth')->name('posts.index');
+
+require __DIR__ . '/auth.php';
