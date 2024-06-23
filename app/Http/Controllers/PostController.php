25f8be53e08c3add58c25db992549cde6bd0701a
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-06-23 17:41:09 +0900

    Postモデルの作成、ddの動作確認

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -9,6 +11,10 @@ class PostController extends Controller
 {
     public function index(): View
     {
+        $user = new User();
+        dump($user->posts());
+        $post = new Post();
+        dd($post->user());
         return view('posts.index');
     }
 }
